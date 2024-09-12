932e89fc4c4760de55e496f2bb89b3b7b23c2556
Krima Madhikarmi <madhikrima20@gmail.com>
2024-09-12 07:41:04 +0545

    user grid

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,26 +1,43 @@
 import { useEffect, useState } from 'react';
-import '../styles/user.css';
+import '../styles/user.css'
 import Card from './userCard';
 
-const Users = () => {
-  const [allUsers, setAllUsers] = useState([]);
-  useEffect(() => {
-    fetch('http://jsonplaceholder.typicode.com/users').then(async (response) => {
-      const users = await response.json();
-      console.log(users);
-      setAllUsers(() => allUsers);
-    });
-  }, []);
-  return (
-    <div id="users">
-      <h1 id="title">All Users</h1>
-      <div id="usergrid">
-        {console.log('users', allUsers)}
-        {allUsers.map((user) => {
-          return <Card />;
-        })}
-      </div>
-    </div>
-  );
-};
+const Users = ()=>{
+
+    const [allUsers,setAllUsers] = useState([]);
+
+    //if set to empty array, called once in each render
+    useEffect(
+        ()=>{
+            //fetch vaneko promise type return garchha i.e async/await tesaile then.
+            fetch("https://jsonplaceholder.typicode.com/users").then(
+                async (response)=>{
+                    const users = await response.json()
+                    console.log(users);
+                    setAllUsers(users)
+                }
+            )
+        },
+        []
+    )
+    return(
+      <div id='container'>
+        <div id='users'>
+            <h1 id='title'>All Users</h1>
+            <div id='user-grid'>
+                {
+                    allUsers.map(
+                        (user)=>{
+                          return (
+                            <Card user={user}/>
+                          )
+                        }
+                    )
+                }
+            </div>
+        </div>
+        </div>
+    );
+}
+
 export default Users;
--- src/pages/userCard.jsx
+++ src/pages/userCard.jsx
@@ -1,37 +1,43 @@
 import '../styles/userCard.css';
 
-const Card = (user) => {
+const Card = ({user}) => {
   return (
-    <div className="card">
-      <div className="nameTag">
-        <h2>{}</h2>
-        <h3>@Krima</h3>
+    <>
+      <div id="box1">
+        <div id="header">
+          <h1>{user.name}</h1>
+          <h3>@{user.username}</h3>
+        </div>
+        <div>
+          <div id="section1">
+            <p>
+              <strong>Email:</strong>{user.email}
+            </p>
+            <p>
+              <strong>Phone:</strong>{user.phone}
+            </p>
+            <p>
+              <strong>Website:</strong>{user.website}
+            </p>
+          </div>
+
+          <div id="section2">
+            <p>
+              <strong>Address:</strong>
+              {user.address.street}
+            </p>
+          </div>
+          <div id="company">
+            <strong>Company:</strong>
+            <p>{user.company.name}</p>
+            <strong>Catchphrase:</strong>
+            <p>{user.company.catchPhrase}</p>
+            <strong>Business:</strong>
+            <p>{user.company.bs}</p>
+          </div>
+        </div>
       </div>
-      <div className="intro">
-        <div>
-          <p>
-            <strong>Email:</strong> krima@gmail.com
-          </p>
-          <p>
-            <strong>Phone:</strong> 123456
-          </p>
-          <p>
-            <strong>Website:</strong> krima1234
-          </p>
-        </div>
-        <div>
-          <strong>Address:</strong> Suryabinayak,Bhaktapur
-        </div>
-        <div>
-          <strong>Company:</strong> 
-          <p>Romagurea-Crona</p>
-          <strong> Catchphrase:</strong>
-          <p>"MultiLayered"</p>
-          <strong> Busniess:</strong>
-          <p> Harness e-mark</p>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 export default Card;
